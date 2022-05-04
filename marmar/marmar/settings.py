5811777baeb1835cb43350cb8f6f8c02fef88a93
davranaff <95549412+davranaff@users.noreply.github.com>
2022-05-04 23:13:42 +0300

    update settings.py

--- marmar/marmar/settings.py
+++ marmar/marmar/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fdphpjwt^a_op938q&v-=95e_3lqz$4v9phn4xn6afo7u)e-@l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'marmar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'marmar',
+        'USER' : 'super',
+        'PASSWORD' : 'Jasur9099',
+        'HOST' : 'marmar-2650.postgres.pythonanywhere-services.com',
+        'PORT' : '12650',
     }
 }
 
